29f4f123aef848e3d482e755ea9d7c9c2a48e488
adithya m <madithyam16@gmail.com>
2022-01-28 23:11:41 +0530

    react 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and yos.
+          Edit <code>src/App.js</code> and save yos.
         </p>
         <a
           className="App-link"
