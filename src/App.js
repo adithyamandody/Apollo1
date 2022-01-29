447e7f0f3c96912961607da78803d0728b389df6
adithya m <madithyam16@gmail.com>
2022-01-29 11:04:00 +0530

    Landing-page-01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import LandingPg from "./LandingPg/Landingpg.jsx";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save yos.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPg/>
     </div>
   );
 }
